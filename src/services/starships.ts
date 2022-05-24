7246481b89d5693a722ed2f40b72d7aef22c9651
Shafeeque O M <shafeequeom7@gmail.com>
2022-05-25 00:02:59 +0400

    Starwar design changes

--- src/services/starships.ts
+++ src/services/starships.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import type { ListStarshipTypes, StarshipTypes } from '@/types/Starship.interface'
 
-export const listStarships = async () => {
+export const listStarships = async (): Promise<ListStarshipTypes> => {
     return await axios.get('https://swapi.dev/api/starships').then(res => res.data)
 }
 
-export const getStarshipDetails = async (id: number) => {
+export const getStarshipDetails = async (id: Number): Promise<StarshipTypes> => {
     return await axios.get(`https://swapi.dev/api/startships/${id}`).then(res => res.data)
 }
 
